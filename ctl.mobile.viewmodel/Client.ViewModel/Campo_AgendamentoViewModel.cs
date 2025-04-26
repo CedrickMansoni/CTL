c254df848074b173e904ead85f3526741bc2d08b
CedrickMansoni <cedrick.k.mansoni@gmail.com>
2025-04-26 12:00:25 +0100

    ...

--- ctl.mobile.viewmodel/Client.ViewModel/Campo_AgendamentoViewModel.cs
+++ ctl.mobile.viewmodel/Client.ViewModel/Campo_AgendamentoViewModel.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.ObjectModel;
 using System.Text.Json;
+using System.Windows.Input;
 using ctl.share.Dominio_App;
 using ctl.share.DTO_App.Campo;
+using ctl.share.DTO_App.Marcacao;
 
 namespace ctl.mobile.viewmodel.Client.ViewModel;
 
@@ -37,8 +40,28 @@ public class Campo_AgendamentoViewModel : BindableObject
         set
         {
             campo = value;
-            
+
             OnPropertyChanged(nameof(Campo));
         }
     }
+
+    private ObservableCollection<Listar_Marcacao_DTO> marcacao = [];
+    public ObservableCollection<Listar_Marcacao_DTO> Marcacao
+    {
+        get => marcacao;
+        set
+        {
+            marcacao = value;
+            OnPropertyChanged(nameof(Marcacao));
+        }
+    }
+
+    public ICommand ListarMarcacoesCommand => new Command<int>(async idCampo =>
+    {
+        int p = Marcacao.Count == 0 ? 0 : 1;
+        var response = await client.GetAsync($"listar/marcacoes?idCampo={idCampo}&skip={Marcacao.Count + 30 * p}&take={30}");
+        if (!response.IsSuccessStatusCode) return;
+        using var m = await response.Content.ReadAsStreamAsync();
+        Marcacao = JsonSerializer.Deserialize<ObservableCollection<Listar_Marcacao_DTO>>(m, options) ?? [];
+    });
 }
