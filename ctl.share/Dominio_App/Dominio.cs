80807377dcb6239eaca007421d89c5a700df862f
CedrickMansoni <cedrick.k.mansoni@gmail.com>
2025-05-21 14:31:45 +0100

    ...

--- ctl.share/Dominio_App/Dominio.cs
+++ ctl.share/Dominio_App/Dominio.cs
@@ -7,6 +7,6 @@ public static class Dominio
     public static string URLApp { get; private set; }
     static Dominio()
     {
-        URLApp = "http://192.168.1.82:5130/";
+        URLApp = "http://172.20.10.2:5130/";
     }
 }
